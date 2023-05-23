991cc15a7eac0ac48546aa9caefd56c633335fc7
Brenno Peralta <brenno.peraltaa@gmail.com>
2023-05-22 20:26:15 -0400

    fix: fix bug in dark mode

--- js/mode.js
+++ js/mode.js
@@ -3,5 +3,4 @@ const checkbox = document.querySelector(".checkbox");
 checkbox.addEventListener("click", () => {
   const body = document.body;
     body.classList.toggle("dark-mode");
-    alert("js work")
 });
